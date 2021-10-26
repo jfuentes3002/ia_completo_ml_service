3da92ff1f918549ed8dc2b18a73e2c9393e9651c
jfuentes3002 <jfuentes3002@gmail.com>
2021-10-26 14:42:35 -0300

    Agregando vista de prediccion.

--- ia_completo/django_ia/apps/endpoints/views.py
+++ ia_completo/django_ia/apps/endpoints/views.py
@@ -54,3 +54,58 @@ def homePageView(request):
     html = '<h2>Bienvenido a Sitio Web de Implementación de modelos de aprendizaje automático con Django</h2>'
     html += '<p>Para acceder a la sección de API, haga clic <a href="http://127.0.0.1:8000/api/v1/" target="_blank">aquí</a></p>'
     return HttpResponse(html)
+
+# please add imports
+import json
+from numpy.random import rand
+from rest_framework import views, status
+from rest_framework.response import Response
+from apps.ml.registry import MLRegistry
+from django_ia.wsgi import registry
+
+'''
+... the rest of the backend/server/apps/endpoints/views.py file ...
+'''
+
+class PredictView(views.APIView):
+    def post(self, request, endpoint_name, format=None):
+
+        algorithm_status = self.request.query_params.get("status", "production")
+        algorithm_version = self.request.query_params.get("version")
+
+        algs = MLAlgorithm.objects.filter(parent_endpoint__name = endpoint_name, status__status = algorithm_status, status__active=True)
+
+        if algorithm_version is not None:
+            algs = algs.filter(version = algorithm_version)
+
+        if len(algs) == 0:
+            return Response(
+                {"status": "Error", "message": "ML algorithm is not available"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if len(algs) != 1 and algorithm_status != "ab_testing":
+            return Response(
+                {"status": "Error", "message": "ML algorithm selection is ambiguous. Please specify algorithm version."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        alg_index = 0
+        if algorithm_status == "ab_testing":
+            alg_index = 0 if rand() < 0.5 else 1
+
+        algorithm_object = registry.endpoints[algs[alg_index].id]
+        prediction = algorithm_object.compute_prediction(request.data)
+
+
+        label = prediction["label"] if "label" in prediction else "error"
+        ml_request = MLRequest(
+            input_data=json.dumps(request.data),
+            full_response=prediction,
+            response=label,
+            feedback="",
+            parent_mlalgorithm=algs[alg_index],
+        )
+        ml_request.save()
+
+        prediction["request_id"] = ml_request.id
+
+        return Response(prediction)
